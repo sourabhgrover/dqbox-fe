c452c1b69d0690d4e532ff41d51cee75f788e485
sourabhgrover <sourabhgrover29@gmail.com>
2024-06-30 17:32:17 +0530

    Dashboard Layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { Outlet, Navigate, useRoutes } from 'react-router-dom';
+import DashboardLayout from '../layouts/dashboard';
 // import PageNotFound from '../pages/page-not-found';
 
 // import DashboardLayout from 'src/layouts/dashboard';
@@ -8,6 +9,8 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 // export const BlogPage = lazy(() => import('src/pages/blog'));
 // export const UserPage = lazy(() => import('src/pages/user'));
 export const Login = lazy(() => import('../pages/login'));
+// export const Dashboard = lazy(() => import('../pages/Dashboard'));
+export const Home = lazy(() => import('../pages/home'));
 // export const ProductsPage = lazy(() => import('src/pages/products'));
 export const PageNotFound = lazy(() => import('../pages/page-not-found'));
 
@@ -15,14 +18,34 @@ export const PageNotFound = lazy(() => import('../pages/page-not-found'));
 
 export default function Router() {
 const routes = useRoutes([
-    {
-        path: '/',
-        element: <Login />,
-    },
+    // {
+    //     path: '/',
+    //     element: <Login />,
+    // },
     {
       path: '/login',
       element: <Login />,
     },
+  //   {
+  //     path: '/dashboard',
+  //     // element: <Dashboard />,
+  //     element: <DashboardLayout />,
+  // },
+  {
+    element: (
+      <DashboardLayout>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </DashboardLayout>
+    ),
+    children: [
+      { element: <Home />, index: true },
+      // { path: 'user', element: <UserPage /> },
+      // { path: 'products', element: <ProductsPage /> },
+      // { path: 'blog', element: <BlogPage /> },
+    ],
+  },
     {
       path: '/404',
       element: <PageNotFound />,
