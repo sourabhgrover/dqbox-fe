4a75054b26c7d7ebbd7d79efe5d6f2028391b21c
sourabhgrover <sourabhgrover29@gmail.com>
2024-07-19 10:59:33 +0530

    Custom Modal

--- src/components/Settings/ManageDQRules.jsx
+++ src/components/Settings/ManageDQRules.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ManageDqRulesTable from './ManageDqRulesTable'
+import CustomModal from '../common/CustomModal'
 
 const ManageDQRules = () => {
   return (
+    <>
     <ManageDqRulesTable />
+    <CustomModal />
+    </>
   )
 }
 
