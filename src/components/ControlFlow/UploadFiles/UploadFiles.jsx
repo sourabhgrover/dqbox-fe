81a73b78936a5abc029f2140a2f23ddaa1b09c0b
sourabhgrover <sourabhgrover29@gmail.com>
2024-07-21 14:45:07 +0530

    Upload Files

--- src/components/ControlFlow/UploadFiles/UploadFiles.jsx
+++ src/components/ControlFlow/UploadFiles/UploadFiles.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const UploadFiles = () => {
+import CustomModal from '../../common/CustomModal'
+import UploadForm from './UploadForm'
+import UploadFilesTable from './UploadFilesTable'
+
+const ManageDefaultingRules = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   return (
-    <div>UploadFiles</div>
+    <div className="px-4 sm:px-6 lg:px-8">
+    <div className="sm:flex sm:items-center">
+      <div className="sm:flex-auto">
+        <h1 className="text-base font-semibold leading-6 text-gray-900">
+          Manage Upload Files
+        </h1>
+      </div>
+      <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+        <button
+          type="button"
+          onClick={handleOpen}
+          className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Upload Files
+        </button>
+      </div>
+    </div>
+  <UploadFilesTable />
+  <CustomModal open={open} onClose={handleClose}>
+    <UploadForm />
+  </CustomModal>
+  </div>
   )
 }
 
-export default UploadFiles
+export default ManageDefaultingRules
+
