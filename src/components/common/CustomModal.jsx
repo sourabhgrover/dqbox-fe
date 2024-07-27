693b2fe692760a87652be26d32182203bfc4df4a
sourabhgrover <sourabhgrover29@gmail.com>
2024-07-27 14:30:12 +0530

    Modal fixes

--- src/components/common/CustomModal.jsx
+++ src/components/common/CustomModal.jsx
@@ -9,7 +9,7 @@ export default function CustomModal({ open, onClose, children }) {
 
   return (
     // <Dialog open={open} onClose={onClose} className="relative z-10">
-    <Dialog open={open} onClose={() => {}} className="relative z-10">
+    <Dialog open={open} onClose={() => {}} className="relative z-50">
       <DialogBackdrop
         transition
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
