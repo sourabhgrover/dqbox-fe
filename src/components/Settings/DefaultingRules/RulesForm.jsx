1baf85c64ebd72afc8a23ae3f4ecee0d7c566116
sourabhgrover <sourabhgrover29@gmail.com>
2024-07-27 13:53:25 +0530

    Upload Form

--- src/components/Settings/DefaultingRules/RulesForm.jsx
+++ src/components/Settings/DefaultingRules/RulesForm.jsx
@@ -33,7 +33,7 @@ export default function RulesForm() {
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
-            Add New DQ KPIs
+            Add New Defaulting Rule
           </h2>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
