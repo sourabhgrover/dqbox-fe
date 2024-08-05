06fddac3070450eca9c4458fadebeac65a4c6dc5
sourabhgrover <sourabhgrover29@gmail.com>
2024-08-05 15:08:11 +0530

    feat: Add forms for managing users, rules, roles, and DQRules

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -21,26 +21,16 @@ import {
   UsersIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
 import TopBar from './TopBar'
 
-const navigation = [
+const initialNavigation = [
   { name: 'DQScore', href: '#', icon: HomeIcon, current: true },
   { name: 'DQDrill', href: '#', icon: UsersIcon, current: false },
   { name: 'Compare', href: '#', icon: FolderIcon, current: false },
   { name: 'Set Targets', href: 'set-targets', icon: CalendarIcon, current: false },
   { name: 'Settings', href: 'settings', icon: DocumentDuplicateIcon, current: false },
   { name: 'Control Flow', href: 'control-flow', icon: ChartPieIcon, current: false },
-]
-const teams = [
-  { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-  { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-  { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
-]
-const userNavigation = [
-  { name: 'Your profile', href: '#' },
-  { name: 'Sign out', href: 'login' },
 ]
 
 function classNames(...classes) {
@@ -49,6 +39,16 @@ function classNames(...classes) {
 
 export default function DashboardLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [navigation, setNavigation] = useState(initialNavigation);
+
+  const handleNavigationClick = (name) => {
+    setSidebarOpen(false)
+    setNavigation((prevNavigation) =>
+      prevNavigation.map((item) =>
+        item.name === name ? { ...item, current: true } : { ...item, current: false }
+      )
+    );
+  };
 
   return (
     <>
@@ -107,6 +107,7 @@ export default function DashboardLayout({ children }) {
                                   : 'text-gray-400 hover:bg-gray-800 hover:text-white',
                                 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
                               )}
+                              onClick={() => handleNavigationClick(item.name)}
                             >
                               <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
                               {item.name}
@@ -115,29 +116,6 @@ export default function DashboardLayout({ children }) {
                         ))}
                       </ul>
                     </li>
-                    {/* <li>
-                      <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                      <ul role="list" className="-mx-2 mt-2 space-y-1">
-                        {teams.map((team) => (
-                          <li key={team.name}>
-                            <a
-                              href={team.href}
-                              className={classNames(
-                                team.current
-                                  ? 'bg-gray-800 text-white'
-                                  : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                                'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
-                              )}
-                            >
-                              <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                {team.initial}
-                              </span>
-                              <span className="truncate">{team.name}</span>
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </li> */}
                     <li className="mt-auto">
                       <a
                         href="#"
@@ -155,7 +133,7 @@ export default function DashboardLayout({ children }) {
         </Dialog>
 
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
@@ -171,18 +149,6 @@ export default function DashboardLayout({ children }) {
                   <ul role="list" className="-mx-2 space-y-1">
                     {navigation.map((item) => (
                       <li key={item.name}>
-                        {/* <a
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-800 text-white'
-                              : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                            'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
-                          )}
-                        >
-                          <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                          {item.name}
-                        </a> */}
                         <Link
                           to={item.href}
                           className={classNames(
@@ -191,6 +157,7 @@ export default function DashboardLayout({ children }) {
                               : 'text-gray-400 hover:bg-gray-800 hover:text-white',
                             'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
                           )}
+                          onClick={() => handleNavigationClick(item.name)}
                         >
                           <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
                           {item.name}
@@ -199,29 +166,6 @@ export default function DashboardLayout({ children }) {
                     ))}
                   </ul>
                 </li>
-                {/* <li>
-                  <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? 'bg-gray-800 text-white'
-                              : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                            'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
-                          )}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li> */}
                 <li className="mt-auto">
                   <a
                     href="#"
@@ -236,7 +180,7 @@ export default function DashboardLayout({ children }) {
           </div>
         </div>
 
-        <div className="lg:pl-72">
+        <div className="lg:pl-64">
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
             <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
               <span className="sr-only">Open sidebar</span>
