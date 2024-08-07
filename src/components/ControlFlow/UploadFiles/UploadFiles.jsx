a911545d61e9605f58823d6088be5241074796ea
sourabhgrover <sourabhgrover29@gmail.com>
2024-08-07 12:48:46 +0530

    Replaced all primary-btn

--- src/components/ControlFlow/UploadFiles/UploadFiles.jsx
+++ src/components/ControlFlow/UploadFiles/UploadFiles.jsx
@@ -22,7 +22,7 @@ const ManageDefaultingRules = () => {
         <button
           type="button"
           onClick={handleOpen}
-          className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="primary-btn"
         >
           Upload Files
         </button>
